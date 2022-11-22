383a3eccf77569a699faf7f6800044b767fdad88
Peter Karalekas <peter@karalekas.com>
2022-11-22 09:40:15 -0800

    Bump version to v0.1.2

--- oqpy/__init__.py
+++ oqpy/__init__.py
@@ -14,7 +14,7 @@
 #  limitations under the License.
 ############################################################################
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 from oqpy.classical_types import *
 from oqpy.control_flow import *
 from oqpy.program import *
